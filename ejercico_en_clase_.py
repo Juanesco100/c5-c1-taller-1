ca8df1557df335b6f50ac0dcbeeaac7decdc827b
Juanesco100 <juanse01sebas@gmail.com>
2026-02-24 09:55:49 -0500

    Mi first commit

---
+++ ejercico_en_clase_.py
@@ -0,0 +1,32 @@
+# ejercicio en clase 
+
+registro_persona = input("¿Cuantas personas desea registrar?: ")
+registro_persona = int(registro_persona)
+
+for i in range(registro_persona):
+    nombre = input("Ingresa tu nombre: ")
+    edad = input("ingresa tu edad: ")
+    edad_entero = int(edad)
+    tieneConocimiento = input("Tienes conocimientos basicos de computación? (s/n): ")
+    print("\n")
+
+    if edad_entero > 15 and tieneConocimiento == "s":
+        print("Puede participar en el taller.")
+        print("\n")
+    else:
+        print("No cumple los requisitos.")
+        print("\n")
+
+
+continuar = True
+
+while continuar :
+
+    if edad_entero < 15:
+        print("Edad no validad, es menor.")
+        edad_entero = input("Ingrese nuevamente la edad: ")
+        edad_entero = int(edad_entero)
+    else:
+        print ("Proceso finalizado!...")
+        print("\n")
+        continuar = False
